2961850569bd92bb5ff154a5095b58ee945fe173
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-23 15:22:12 +0200

    Exercise completed

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
  * lo studente avrà  le seguenti proprietà: nome, cognome e età.
  * Stampare attraverso il for..in tutte le proprietà (chiavi e valori)
  */
+
+// Creo oggetto che descrive uno studente
+var infoStudente = {
+    nome: 'Jane',
+    cognome: 'Doe',
+    età: 21
+};
+console.log(infoStudente);
+
+
+// Stampo chiavi e valori dell'oggetto
+for(var key in infoStudente) {
+    console.log(key + ': ' + infoStudente[key]);
+}
