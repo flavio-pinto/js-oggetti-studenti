10c2f50fafee7a1f12ef6ebbf731281e35658357
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-23 15:04:20 +0200

    html/css/js files created

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Creare un oggetto che descriva uno studente
+ * lo studente avrà  le seguenti proprietà: nome, cognome e età.
+ * Stampare attraverso il for..in tutte le proprietà (chiavi e valori)
+ */
