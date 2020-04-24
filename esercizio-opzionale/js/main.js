2363f508fdd2c4ab6801795c978cd54b5a815c94
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-24 16:18:37 +0200

    Initial messages with Handlebars - Completed

--- esercizio-opzionale/js/main.js
+++ esercizio-opzionale/js/main.js
@@ -1,6 +1,9 @@
 /**
  * OPZIONALE
  * Riprodurre la piccola chat fatta insieme durante la lezione utilizzando handlerbars.
+ * Estendere la chat fatta con handlebars inserendo i messaggi iniziali, visibili al caricamento della pagina, generati da un array di oggetti.
+ * Gli oggetti conterranno proprietà come testo e orario
+ * Usiamo Handlebars per la generazione anche di questi messaggi, come avete fatto ieri per i messaggi inseriti dall’utente
  */
 
 $(document).ready(function () {
@@ -18,6 +21,32 @@ $(document).ready(function () {
     var source = $('#message-template').html();
     // Funzione utility che prende source e fa un compile iniziale del template
     var template = Handlebars.compile(source);
+
+    // Inserisci messaggi iniziali presi da un array di oggetti
+    var initialMessagesContainer = [
+        {
+            text: 'sempre caro mi fu quest\'ermo colle',
+            time: '15:00',
+            activeClass: 'sent'
+        },
+
+        {
+            text: 'nel campo mezzo grigio e mezzo nero, resta un aratro senza buoi che pare dimenticato, tra il vapor leggero',
+            time: '15:01',
+            activeClass: 'received'
+        },
+
+        {
+            text: 'ho sceso, dandoti il braccio, almeno un milione di scale',
+            time: '15:02',
+            activeClass: 'sent'
+        }
+    ];
+    // Creo ciclo per aggiungere i messaggi iniziali
+    for(var i = 0; i < initialMessagesContainer.length; i++) {
+        var initialMessage = template(initialMessagesContainer[i]);
+        chat.append(initialMessage);
+    }
 
     // Inserisci nuovo messaggio
     button.click(function() {
@@ -46,7 +75,4 @@ $(document).ready(function () {
             chat.append(newMessage);
         }, 1000);
     });
-
-
-
 }); // fine ready
